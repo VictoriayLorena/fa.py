83689f8c87938fa7f64449114de2f49520b24ea5
VictoriayLorena <48284574+VictoriayLorena@users.noreply.github.com>
2019-03-06 13:59:06 +0100

    Add files via upload

---
+++ fa.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env   pyhton
+# __*__   coding:utf-8  __*__
+def main():
+
+    C = float(input("Grados Celsius: 4"))
+    F = (( 9.0 / 5.0))
+    media = (celsius + fahrenheit) / 2
+    
+    print("La media aritmética de {9.0} y {5.0} es {}".format(numero_1,numero_2,media))
+ 
+if __name__ == "__main__":
+    main()
+
